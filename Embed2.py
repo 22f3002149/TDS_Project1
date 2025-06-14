88d87c05b56b46350aa476a17e9ce68856831f47
Jolish Chengammal <22f3002149@ds.study.iitm.ac.in>
2025-06-14 13:21:08 +0530

    merged image descriptions

--- Embed2.py
+++ Embed2.py
@@ -11,16 +11,33 @@ import google.generativeai as genai
 from semantic_text_splitter import MarkdownSplitter
 from openai import OpenAI
 
-def get_chunks(file_path: str, chunk_size: int = 15000):
+# Get chunks from a file, splitting the content into manageable pieces.
+def get_chunks(file_path: str, chunk_size: int = 1000):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
-    _overlap = 500
+    _overlap = 200
     splitter = MarkdownSplitter(chunk_size, overlap=_overlap)
     #splitter = MarkdownSplitter(chunk_size)
     chunks = splitter.chunks(content)
     return chunks
 
+# Get embeddings for a given text using OpenAI API.
+def get_embeddings(text: str) -> list:
+    """ Get embeddings for a given text using OpenAI API. """
+    client = OpenAI()
+    response = client.embeddings.create(input=text, model="text-embedding-3-small")
+    embedding = response.data[0].embedding
+    return embedding
+
+
+
+
+
 file_path = "test/archive.md"
 chunks = get_chunks(file_path)
+em = get_embeddings(chunks[3])
+
 print(f"Number of chunks: {len(chunks)}")
-print(f"First chunk: {chunks[1]}")
+print(f"First chunk: {chunks[3]}")
+print(len(em))
+print(f"Embedding for the chunk: {em}")  
